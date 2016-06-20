bf93e8bf989853d44321bd431ad23cf4d869529b
Jason Staten <jstaten07@gmail.com>
2016-06-19 22:30:03 -0600

    rip out express stuff

--- src/core.cljs
+++ src/core.cljs
@@ -4,19 +4,7 @@
 
 (nodejs/enable-util-print!)
 
-(def port 3001)
-(def express (nodejs/require "express"))
-
-(defn render-page []
-  (throw (js/Error. "Kow")))
-
-(defn handle-request [req res]
-  (.send res (render-page)))
-
 (defn -main []
-  (let [app (express)]
-    (.get app "/" handle-request)
-    (.listen app port (fn []
-                        (println (str "Server started on port " port))))))
+  (println "Running" ))
 
 (set! *main-cli-fn* -main)
